ed36d7d85e51076d6c2e133dc14121b8e3d8800e
stone-hem <hemie@protonmail.ch>
2022-10-31 18:54:10 +0300

    minute changes

--- resources/views/authorizer/authorizer-memberspending.blade.php
+++ resources/views/authorizer/authorizer-memberspending.blade.php
@@ -1,81 +1,95 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
+
+<head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="{{ asset('cssFiles/style.css') }}" >
-    <link rel="stylesheet" href="{{ asset('cssFiles/clerkmembers.css') }}" >
+    <link rel="stylesheet" href="{{ asset('cssFiles/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('cssFiles/clerkmembers.css') }}">
     <!-- Boxicons CDN Link -->
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   </head>
-   
-    <body>
-        @include('layouts.nav')
-        @section('navigation')
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
 
-    @include('layouts.authorizer-sidebar')
+<body>
+    @include('layouts.nav')
+    @section('navigation')
 
-     @section('authorizer-sidebar')
+        @include('layouts.authorizer-sidebar')
 
-     @endsection
+    @section('authorizer-sidebar')
 
-      
-      <section class="home-section">
+    @endsection
+
+
+    <section class="home-section">
         <div class="home-content">
-          <div class="container" >
-            <h3 class="top-header"><b>Sacco Members Pending Approval</b></h3>
-            <div class="table">
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Id</th>
-                            <th>Name</th>
-                            <th>Email</th>
-                            <th>Id number</th>
-                            <th>Location</th>
-                            <th>Gender</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                      @foreach ($member as $item)
-                      <tr>
-                        <td>{{ $item->id }}</td>
-                        <td>{{ $item->name }}</td>
-                        <td>{{ $item->email }}</td>
-                        <td>{{ $item->id_number }}</td>
-                        <td>{{ $item->location }}</td>
-                        <td>
-                           {{ $item->gender }}
-                        </td>
-                        <td>
-                          <div class="button1">
-                            <form action="{{ url('authorizer-approve-member/'.$item->id) }}" method="POST" enctype="multipart/form-data">
-                              @csrf
-                              <input type = "hidden" name = "approve" value = "1" />
-                              <button class="btn2" type="submit" style="background-color: rgb(109, 207, 109);"> Approve Member</button>
-                            </form>
-                           </div>
-                      </td>
-                      <td>
-                          <div class="button1">
-                            <form action="{{ url('authorizer-approve-member/'.$item->id) }}" method="POST" enctype="multipart/form-data">
-                              @csrf
-                              <input type = "hidden" name = "approve" value = "0" />
-                              <button class="btn1" type="submit" style="background-color: rgb(200, 79, 79);" >Reject request</button>
-                            </form>
-                          </div>
-                      </td>
-                    </tr>
-                      @endforeach
-                    </tbody>
-                </table>
+            <div class="container">
+                <h3 class="top-header"><b>Sacco Members Pending Approval</b></h3>
+                <div class="table">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Id</th>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Id number</th>
+                                <th>Location</th>
+                                <th>Gender</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @if (count($member) == 0)
+                                <tr >
+                                  <td colspan="6" style="text-align: center"><b>Hey! No pending members at the moment</b>
+                                  </td></tr>
+                            @else
+                                @foreach ($member as $item)
+                                    <tr>
+                                        <td>{{ $item->id }}</td>
+                                        <td>{{ $item->name }}</td>
+                                        <td>{{ $item->email }}</td>
+                                        <td>{{ $item->id_number }}</td>
+                                        <td>{{ $item->location }}</td>
+                                        <td>
+                                            {{ $item->gender }}
+                                        </td>
+                                        <td>
+                                            <div class="button1">
+                                                <form action="{{ url('authorizer-approve-member/' . $item->id) }}"
+                                                    method="POST" enctype="multipart/form-data">
+                                                    @csrf
+                                                    <input type="hidden" name="approve" value="1" />
+                                                    <button class="btn2" type="submit"
+                                                        style="background-color: rgb(109, 207, 109);"> Approve
+                                                        Member</button>
+                                                </form>
+                                            </div>
+                                        </td>
+                                        <td>
+                                            <div class="button1">
+                                                <form action="{{ url('authorizer-approve-member/' . $item->id) }}"
+                                                    method="POST" enctype="multipart/form-data">
+                                                    @csrf
+                                                    <input type="hidden" name="approve" value="0" />
+                                                    <button class="btn1" type="submit"
+                                                        style="background-color: rgb(200, 79, 79);">Reject
+                                                        request</button>
+                                                </form>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            @endif
+                        </tbody>
+                    </table>
+                </div>
             </div>
+            {{ $member->onEachSide(2)->links() }}
         </div>
-        {{ $member->onEachSide(2)->links() }}
-        </div>
-      </section>
-    
-    
-    
-    </body>
-    </html>
+    </section>
+
+
+
+</body>
+
+</html>
--- resources/views/authorizer/emergency.blade.php
+++ resources/views/authorizer/emergency.blade.php
@@ -43,13 +43,6 @@
 @section('authorizer-sidebar')
 
 @endsection
-
-    {{-- @extends('layouts.authorizer-sidebar')
-    <x-app-layout>
-
-    </x-app-layout>
-
-    @section('authorizer-sidebar') --}}
         <section class="home-section">
             <div class="home-content">
                 <div class="space-content">
@@ -142,24 +135,6 @@
                 {{ $emergency_loans->onEachSide(2)->links() }}
             </div>
         </section>
-        <script >
-          // Get the container element
-    var btnContainer = document.getElementById("myDIV");
-    
-    // Get all buttons with class="btn" inside the container
-    var btns = btnContainer.getElementsByClassName("navigate");
-    
-    // Loop through the buttons and add the active class to the current/clicked button
-    for (var i = 0; i < btns.length; i++) {
-      btns[i].addEventListener("click", function() {
-        var current = document.getElementsByClassName("active");
-        current[0].className = current[0].className.replace(" active", "");
-        this.className += " active";
-      });
-    } 
-        </script>
-    {{-- @endsection --}}
-
 
 </body>
 
