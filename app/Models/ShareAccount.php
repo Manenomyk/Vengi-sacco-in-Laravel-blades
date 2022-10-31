f8867ba9179561fdef3ce3b58ce464570f26cab9
stone-hem <hemie@protonmail.ch>
2022-10-31 18:25:13 +0300

    logging using spatie

--- app/Models/ShareAccount.php
+++ app/Models/ShareAccount.php
@@ -2,7 +2,9 @@
 
 namespace App\Models;
 
+use Spatie\Activitylog\LogOptions;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
@@ -10,4 +12,10 @@ class ShareAccount extends Model
 {
     use HasFactory;
     use SoftDeletes;
+    use LogsActivity;
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults();
+    }
 }
